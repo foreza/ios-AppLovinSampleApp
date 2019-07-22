dbf508a3e395d5bbe53821f79a6183d1bbe8efde
Jason Chiu <jforeza@gmail.com>
2019-07-22 00:50:13 -0700

    Add interstitial zones 1 and 2 implementation
    interstitial 1 will load + show
    interstitial 2 will preload and then allow you to show
    implemented buttons, helpful text, and hide/show button depending on ad load state
    fixed view so that it scales with screen width and height

--- AppLovinSample/FirstViewController.h
+++ AppLovinSample/FirstViewController.h
@@ -9,7 +9,7 @@
 #import <UIKit/UIKit.h>
 #import <AppLovinSDK/AppLovinSDK.h>
 
-@interface FirstViewController : UIViewController <ALAdLoadDelegate, ALAdDisplayDelegate, ALAdViewEventDelegate>
+@interface FirstViewController : UIViewController <ALAdLoadDelegate, ALAdDisplayDelegate, ALAdViewEventDelegate, ALAdVideoPlaybackDelegate>
 
 
 // Todo: implement
@@ -18,6 +18,10 @@
 @property (strong, nonatomic) ALAdView *adView;     // Banner adView            1f4255944afd12df
 @property (strong, nonatomic) ALAd *adIntZone1;     // Interstitial Ad Zone     7317b8287164ccb9
 @property (strong, nonatomic) ALAd *adIntZone2;     // Interstitial Ad Zone     855c5b0b8889a103
+
+@property (weak, nonatomic) IBOutlet UIButton *buttonLoadShow;      // Unused
+@property (weak, nonatomic) IBOutlet UIButton *buttonPreload;
+@property (weak, nonatomic) IBOutlet UIButton *buttonShow;
 
 @end
 
--- AppLovinSample/FirstViewController.m
+++ AppLovinSample/FirstViewController.m
@@ -20,17 +20,18 @@ NSString *bannerZoneID = @"1f4255944afd12df";
 NSString *int1ZoneID = @"7317b8287164ccb9";
 NSString *int2ZoneID = @"855c5b0b8889a103";
 
-Boolean interstitial1Loaded = false;
-Boolean interstitial2Loaded = false;
+Boolean interstitial2Loaded = false;        // The 2nd interstitial will be load + show
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
     
     [self initAndLoadBanner];
+    [self changeUIStateForInterstitialNotReady];
 }
 
 
+// Method to init and load the banner for a zone
 - (void) initAndLoadBanner {
     
     self.adView = [[ALAdView alloc] initWithSize: [ALAdSize sizeBanner] zoneIdentifier: bannerZoneID];
@@ -49,17 +50,77 @@ Boolean interstitial2Loaded = false;
 }
 
 
+
+// Method to load an interstitial with a provided zone ID
+- (void) loadInterstitialFor:(NSString *)zoneID {
+    // Load an interstitial ad and be notified when the ad request is finished.
+    [[ALSdk shared].adService loadNextAdForZoneIdentifier:zoneID andNotify: self];
+    [ALInterstitialAd shared].adDisplayDelegate = self;
+    [ALInterstitialAd shared].adVideoPlaybackDelegate = self;
+}
+
+
+
+
+#pragma mark - <IBAction Methods>
+
+- (IBAction)actionLoadAndShowInterstitial:(id)sender {
+    [self loadInterstitialFor:int1ZoneID];
+}
+
+- (IBAction)actionPreloadInterstitial:(id)sender {
+    [self loadInterstitialFor:int2ZoneID];
+}
+
+- (IBAction)actionShowPreloadedInterstitial:(id)sender {
+    if (interstitial2Loaded) {
+        [[ALInterstitialAd shared] showAd: self.adIntZone2];
+    }
+}
+
+
+
+
+#pragma mark - <UI Methods Methods>
+
+- (void) changeUIStateForInterstitialNotReady {
+    [self.buttonShow setHidden:true];
+    [self.buttonPreload setHidden:false];
+}
+
+- (void) changeUIStateForInterstitialIsReady {
+    [self.buttonShow setHidden:false];
+    [self.buttonPreload setHidden:true];
+}
+
 #pragma mark - <ALAdLoadDelegate Methods>
 
 - (void)adService:(ALAdService *)adService didLoadAd:(ALAd *)ad {
+    
+    
     NSString *msg = [NSString stringWithFormat:@"%@%@%@", kLogTag, @" adService didLoadAd for: ", ad.zoneIdentifier];
     NSLog(@"%@", msg);
-
+    
+    // If this is the first interstitial, show it immediately
+    if ([ad.zoneIdentifier isEqualToString:int1ZoneID]) {
+        self.adIntZone1 = ad;
+        [[ALInterstitialAd shared] showAd: ad];
+    }
+    
+    // If this is the second interstitial, indicate it is ready to be shown
+    else if ([ad.zoneIdentifier isEqualToString:int2ZoneID]) {
+        self.adIntZone2 = ad;
+        interstitial2Loaded = true;
+        [self changeUIStateForInterstitialIsReady];
+//        [[ALInterstitialAd shared] showAd: ad];
+    }
+    
 }
 
 - (void)adService:(ALAdService *)adService didFailToLoadAdWithError:(int)code {
     NSString *msg = [NSString stringWithFormat:@"%@%@%d", kLogTag, @" adService didFailToLoadAdWithError with: ", code];
     NSLog(@"%@", msg);
+
 }
 
 
@@ -70,6 +131,13 @@ Boolean interstitial2Loaded = false;
 - (void)ad:(ALAd *)ad wasDisplayedIn:(UIView *)view {
     NSString *msg = [NSString stringWithFormat:@"%@%@%@", kLogTag, @" wasDisplayedIn for: ", ad.zoneIdentifier];
     NSLog(@"%@", msg);
+    
+    // Control hide/show behavior
+    if ([ad.zoneIdentifier isEqualToString:int2ZoneID]) {
+        interstitial2Loaded = false;                        // Set this to false
+        [self changeUIStateForInterstitialNotReady];
+    }
+    
 }
 
 - (void)ad:(ALAd *)ad wasHiddenIn:(UIView *)view {
@@ -111,6 +179,21 @@ Boolean interstitial2Loaded = false;
 - (void)ad:(ALAd *)ad didFailToDisplayInAdView:(ALAdView *)adView withError:(ALAdViewDisplayErrorCode)code {
     NSString *msg = [NSString stringWithFormat:@"%@%@%ld", kLogTag, @" didFailToDisplayInAdView with: ", (long)code];
     NSLog(@"%@", msg);
+}
+
+
+#pragma mark - <ALAdVideoPlaybackDelegate Methods>
+
+
+- (void)videoPlaybackBeganInAd:(ALAd *)ad{
+    NSString *msg = [NSString stringWithFormat:@"%@%@%@", kLogTag, @" videoPlaybackBeganInAd for: ", ad.zoneIdentifier];
+    NSLog(@"%@", msg);
+}
+
+- (void)videoPlaybackEndedInAd:(ALAd *)ad atPlaybackPercent:(NSNumber *)percentPlayed fullyWatched:(BOOL)wasFullyWatched {
+    NSString *msg = [NSString stringWithFormat:@"%@%@%@", kLogTag, @" videoPlaybackEndedInAd: ", ad.zoneIdentifier];
+    NSLog(@"%@", msg);
+    
 }
 
 
